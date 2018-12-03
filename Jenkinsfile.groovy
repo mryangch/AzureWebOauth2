f7cec21110d5e8fca5cbcb8c83f5d6bf71aa6721
Chunhai Yang <mryangch001@gmail.com>
2018-12-03 18:42:06 +0800

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,7 +3,7 @@ pipeline {
 	
     stages {
         stage('download') {
-	    checkout scm
+	   
             steps {
                 //echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}"
 				echo "downloading codes from GitHub"
