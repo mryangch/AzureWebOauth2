1118ba137123cc00248dafa646eb5d524f664bea
Chunhai Yang <mryangch001@gmail.com>
2018-12-04 17:58:37 +0800

    Update Jenkinsfile_script.groovy

--- Jenkinsfile_script.groovy
+++ Jenkinsfile_script.groovy
@@ -6,7 +6,7 @@ node {
     stage('Build') {
         echo 'Build project...'
         bat 'nuget restore AzureWebOAuth2.sln'
-		bat "\"${tool 'MSBuild'}\" AzureWebOAuth2.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /p:ProductVersion=1.0.0.${env.BUILD_NUMBER}"
+	bat "\"${tool 'MSBuild'}\" AzureWebOAuth2.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /p:ProductVersion=1.0.0.${env.BUILD_NUMBER}"
     }
     stage('Archive') {
         echo 'Run test cases...'
